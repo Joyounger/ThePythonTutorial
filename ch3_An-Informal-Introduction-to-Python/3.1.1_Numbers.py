bc786343f90d44c54303a80915acf897cd04f734
Joyounger <942510346@qq.com>
2018-02-12 23:48:33 +0800

    update
    Signed-off-by: Joyounger <942510346@qq.com>

---
+++ ch3_An-Informal-Introduction-to-Python/3.1.1_Numbers.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+
+
+print(8 / 5)  # division always returns a floating point number
+
+
+# To do floor division and get an integer result (discarding any fractional result) you can use the // operator
+print(17 // 3)
